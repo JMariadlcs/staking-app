76f0f201a8f461e8b8ce0c34204cb2383a356777
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-04-28 14:18:46 +0200

    tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
+require("@nomiclabs/hardhat-waffle")
+// require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+// require("solidity-coverage")
+// require("hardhat-gas-reporter")
+// require("hardhat-contract-sizer")
+// require("./tasks")
+// require("@appliedblockchain/chainlink-plugins-fund-link")
+// require("dotenv").config()
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.7",
+  namedAccounts: {
+    deployer: { // deployer account
+      default: 0, // ethers built an account at index 0
+    }
+  }
 };
